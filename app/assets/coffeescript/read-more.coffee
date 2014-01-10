add3f935c07d4cb153ab7896bd1342b52f379083
BryanCunningham <bryan.cunningham@me.com>
2014-01-10 16:04:23 -0500

    reworking javascript to be more performant. Also set read-more-container height dynamically

--- app/assets/coffeescript/read-more.coffee
+++ app/assets/coffeescript/read-more.coffee
@@ -1,39 +1,40 @@
-featureHeight = ->
-      accumulator = 0
-      $('.feature').each ->
-        accumulator += $(this).outerHeight(true)
-      console.log accumulator
-      return accumulator
+$(document).ready ->
 
-$ ->
+  featureHeight = ($elements) ->
+    accumulator = 0
+    $elements.each ->
+      accumulator += $(this).outerHeight(true)
+    return accumulator
 
-  window.LP = (->
+  initialHeight = ->
+    h = 0
+    height = $('.feature').slice(0,2)
+    height.each ->
+      h += $(this).outerHeight(true)
+    return h
 
-    initialHeight = 400
+  $('.read-more-container').css('height', initialHeight())
+  $(document).on 'click', '.readmore', ->
+  # handlers don't attach on page load
+    $readMoreContainer = $('.read-more-container')
+    # cached selector. Only crawls DOM once to find matching nodes.
+    if $readMoreContainer.height() > initialHeight()
+      $readMoreContainer.animate(
+        height: initialHeight()
+      , 500)
+      $(this).text('Read more')
+    else
+      $readMoreContainer.animate(
+        height: featureHeight($('.feature'))
+      , 500)
+      $(this).text('Read less')
+    false
 
-    return {
 
-      open : $('.readmore').on 'click', ->
-        if $('.read-more-container').height() > 400
-          $('.read-more-container').animate(
-            height: initialHeight
-          , 500)
-          $(this).text('Read more')
-        else
-          $('.read-more-container').animate(
-            height: featureHeight()
-          , 500)
-          $(this).text('Read less')
-        false
+  $(document).on 'click', '.js-img', ->
+    image = $('<img/>').addClass('lightboxImage').attr('src', $(this).attr('src'))
+    lightboxContainer = $('<div/>').addClass('lightbox').append(image)
 
-
-      lightbox : $('.images-container').on 'click', 'img', ->
-        image = $('<img/>').addClass('lightboxImage').attr('src', $(this).attr('src'))
-        lightboxContainer = $('<div/>').addClass('lightbox').append(image)
-
-        $('body').append(lightboxContainer).on 'keyup', (e) ->
-          if e.keyCode is 13 or e.keyCode is 27
-            $('.lightbox').remove()
-
-    }
-  )()
+    $('body').append(lightboxContainer).on 'keyup', (e) ->
+      if e.keyCode is 13 or e.keyCode is 27
+        $('.lightbox').remove()
--- public/js/read-more.js
+++ public/js/read-more.js
@@ -1,50 +1,55 @@
 (function() {
-  var featureHeight;
+  $(document).ready(function() {
+    var featureHeight, initialHeight;
 
-  featureHeight = function() {
-    var accumulator;
+    featureHeight = function($elements) {
+      var accumulator;
 
-    accumulator = 0;
-    $('.feature').each(function() {
-      return accumulator += $(this).outerHeight(true);
+      accumulator = 0;
+      $elements.each(function() {
+        return accumulator += $(this).outerHeight(true);
+      });
+      return accumulator;
+    };
+    initialHeight = function() {
+      var h, height;
+
+      h = 0;
+      height = $('.feature').slice(0, 2);
+      height.each(function() {
+        return h += $(this).outerHeight(true);
+      });
+      return h;
+    };
+    $('.read-more-container').css('height', initialHeight());
+    $(document).on('click', '.readmore', function() {
+      var $readMoreContainer;
+
+      $readMoreContainer = $('.read-more-container');
+      if ($readMoreContainer.height() > initialHeight()) {
+        $readMoreContainer.animate({
+          height: initialHeight()
+        }, 500);
+        $(this).text('Read more');
+      } else {
+        $readMoreContainer.animate({
+          height: featureHeight($('.feature'))
+        }, 500);
+        $(this).text('Read less');
+      }
+      return false;
     });
-    console.log(accumulator);
-    return accumulator;
-  };
+    return $(document).on('click', '.js-img', function() {
+      var image, lightboxContainer;
 
-  $(function() {
-    return window.LP = (function() {
-      var initialHeight;
-
-      initialHeight = 400;
-      return {
-        open: $('.readmore').on('click', function() {
-          if ($('.read-more-container').height() > 400) {
-            $('.read-more-container').animate({
-              height: initialHeight
-            }, 500);
-            $(this).text('Read more');
-          } else {
-            $('.read-more-container').animate({
-              height: featureHeight()
-            }, 500);
-            $(this).text('Read less');
-          }
-          return false;
-        }),
-        lightbox: $('.images-container').on('click', 'img', function() {
-          var image, lightboxContainer;
-
-          image = $('<img/>').addClass('lightboxImage').attr('src', $(this).attr('src'));
-          lightboxContainer = $('<div/>').addClass('lightbox').append(image);
-          return $('body').append(lightboxContainer).on('keyup', function(e) {
-            if (e.keyCode === 13 || e.keyCode === 27) {
-              return $('.lightbox').remove();
-            }
-          });
-        })
-      };
-    })();
+      image = $('<img/>').addClass('lightboxImage').attr('src', $(this).attr('src'));
+      lightboxContainer = $('<div/>').addClass('lightbox').append(image);
+      return $('body').append(lightboxContainer).on('keyup', function(e) {
+        if (e.keyCode === 13 || e.keyCode === 27) {
+          return $('.lightbox').remove();
+        }
+      });
+    });
   });
 
 }).call(this);
